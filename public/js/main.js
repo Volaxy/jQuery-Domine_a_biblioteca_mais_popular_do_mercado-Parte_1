8533c0ba246301ffa82c828bfb2ed4d15b6a05f2
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-01-20 13:45:27 -0300

    ADD - Chapter 2 Completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,18 @@
 const phrase = $(".phrase").text();
-// The "$" is a shorthand for jQuery(".phrase")
-// The ".text()" gets the text content of an element
 
 const wordsNumbers = phrase.split(" ").length;
 const $phraseLength = $(".number-of-words");
 
 $phraseLength.text(wordsNumbers);
+
+
+const $typingField = $(".typing-field");
+$typingField.on("input", function() { // The "on()" execute a function when the event type in the 1º parameters is activated
+    const content = $typingField.val(); // The "val()" gets the content value of an element
+    const wordsQuantity = content.split(/\s+/).length - 1;
+
+    $("#counter-words").text(wordsQuantity);
+
+    const charactersQuantity = content.length;
+    $("#counter-characters").text(charactersQuantity);
+});
